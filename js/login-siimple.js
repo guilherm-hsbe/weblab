55e816565c9276858bf665455d5bd77478600f0b
guilherm-hsbe <guilherm3hsb@gmail.com>
2024-01-19 09:47:26 -0300

    feat(js/login-page): Adiciona funções JS de feedback dos inputs

--- js/login-siimple.js
+++ js/login-siimple.js
@@ -8,3 +8,26 @@ function heroSubtitleWriter() {
         setTimeout(heroSubtitleWriter, speed);
     }
 } heroSubtitleWriter();
+function throwPasswordFeedback(message, isInvalid) {
+    const messagePassword = document.getElementById('alertPassword');
+    if (isInvalid == true) {
+        messagePassword.classList.remove("is-hidden");
+        messagePassword.innerHTML = message;
+    }
+    else if (!messagePassword.classList.contains('is-hidden')) {
+        messagePassword.classList.add("is-hidden");
+        messagePassword.innerHTML = null;
+    }
+}
+
+function throwEmailFeedback(message, isInvalid) {
+    const messageEmail = document.getElementById('alertEmail');
+    if (isInvalid == true) {
+        messageEmail.classList.remove("is-hidden");
+        messageEmail.innerHTML = message;
+    }
+    else if (!messageEmail.classList.contains('is-hidden')) {
+        messageEmail.classList.add("is-hidden");
+        messageEmail.innerHTML = null;
+    }
+}
