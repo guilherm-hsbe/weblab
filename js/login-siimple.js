9659d856c14a21e789ef5c25029e560dc26b7ce5
guilherm-hsbe <guilherm3hsb@gmail.com>
2024-01-19 21:01:11 -0300

    feat(js/login-form): Adicona classe de margem ao ícone de sucesso de login do botão

--- js/login-siimple.js
+++ js/login-siimple.js
@@ -27,7 +27,7 @@ function throwValidLogin() {
     loginButton.classList.add('has-bg-success', 'is-disabled');
     loginButtonMessage.innerHTML = 'Logged In'
     loginButtonIcon.classList = []
-    loginButtonIcon.classList.add('fa-solid', 'fa-check', 'fa-bounce');
+    loginButtonIcon.classList.add('fa-solid', 'fa-check', 'fa-bounce', 'has-ml-1');
 
     document.getElementById('userPassword').disabled = true;
     document.getElementById('userEmail').disabled = true;
